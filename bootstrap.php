5b731da595e75a3a57fac495ebce9abc12fd408e
Victor <victor.miletskiy@gmail.com>
2016-04-23 17:59:58 +0300

    Plugin description fix

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: GovRight Corpus Utilities
 Plugin URI: https://github.com/GovRight/wp-corpus-utils
-Description: Utilities for handling common small tasks like locale data extraction or Corpus API calls in Wordpress. Check <a href="https://github.com/GovRight/wp-corpus-utils">the plugin Github page</a> for details and instructions.
+Description: Developer's suite for handling common small tasks like locale data extraction or Corpus API calls in Wordpress. Check <a href="https://github.com/GovRight/wp-corpus-utils">the plugin Github page</a> for details and instructions.
 Version: 1.0.0
 Author: GovRight
 Author URI: http://govright.org/
